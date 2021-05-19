6b90a1c647a0b706609db6c75d28761fda410ebb
Mylene <myklijn@gmail.com>
2021-05-19 11:52:18 +0200

    Added card/post-it module and minimal styling.

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@
         scrumBoard
       }
     }
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/modules/scrumboard/scrumboard.js
@@ -0,0 +1,8 @@
+import card from './../card/card.vue';
+
+export default {
+  name: 'scrumboard',
+  components: {
+    card
+  }
+}
