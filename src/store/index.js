5a63aadbafe554f63041847fb9d71f15ae28d773
Mylene <myklijn@gmail.com>
2021-05-20 11:47:34 +0200

    Added local storage.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 import cards from './modules/cards'
+
+const vuexPersist = new VuexPersist({
+  key: 'scrum-bord',
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   modules: {
     'cards':  cards
   }
