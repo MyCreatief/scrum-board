1c4fd7ceb3bd65afbdb831ecd7f451002b437d66
Mylene <myklijn@gmail.com>
2021-05-20 14:25:29 +0200

    Added E2E test for delete, add, edit and move.

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,30 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('Clean install  First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Clean install')
+describe('E2E Delete, New, Edit, Move', function() {
+
+  it('Test creating, editing, moving and delete', function() {
+
+    cy.viewport(1920, 1089)
+
+    cy.visit('http://localhost:8080/')
+
+    cy.clearLocalStorage()
+
+    cy.get('#app > .container > .grid-content > #\\30 > button').click()
+
+    cy.get('#app > .container > .grid-toolbar > .toolbar > button').click()
+
+    cy.get('#app > .container > .grid-content > #\\30 > div').dblclick()
+
+    cy.get('#app > .container > .grid-content > #\\30 > input').type('new content...test')
+
+    cy.get('body > #app > .container > .grid-content').click()
+
+    cy.get('body > #app > .container > .grid-content > #\\30').trigger('mousedown').trigger('mousemove', { clientX: 800, clientY: 200 })
+
+    cy.get('body > #app > .container > .grid-content > #\\30').click()
+
+
   })
+
 })
